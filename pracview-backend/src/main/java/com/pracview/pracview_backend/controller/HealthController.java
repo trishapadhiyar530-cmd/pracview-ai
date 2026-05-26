782039aa2b873b6c1ace7dc4cef28274ac2f57b4
Trisha Padhiyar <trishapadhiyar770@gmail.com>
2026-05-26 19:24:28 +0530

    Add health endpoint for Render

---
+++ pracview-backend/src/main/java/com/pracview/pracview_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.pracview.pracview_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "PracView backend is live!";
+    }
+}
