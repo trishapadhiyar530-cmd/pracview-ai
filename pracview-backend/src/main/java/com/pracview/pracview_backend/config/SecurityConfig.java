bbe6b4ac4472dab3754ccdaf44c843fb808020c2
Trisha Padhiyar <trishapadhiyar770@gmail.com>
2026-05-27 21:43:29 +0530

    Fix CORS for Vercel frontend

--- pracview-backend/src/main/java/com/pracview/pracview_backend/config/SecurityConfig.java
+++ pracview-backend/src/main/java/com/pracview/pracview_backend/config/SecurityConfig.java
@@ -49,9 +49,14 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of(
+        "http://localhost:5173",
+        "https://pracview-ai.vercel.app",
+        "https://*.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
